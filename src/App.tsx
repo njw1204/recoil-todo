1fa52abd13034d5392080f0edfec4d634b3e40ae
Na Jongwoo <njw1204@naver.com>
2022-06-13 23:34:25 +0900

    react-hook-form 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,15 @@
 import ToDoList from "./component/ToDoList";
+import styled from "styled-components";
+
+const AppContainer = styled.div`
+  padding: 32px 16px;
+`;
 
 function App() {
   return (
-    <>
+    <AppContainer>
       <ToDoList/>
-    </>
+    </AppContainer>
   );
 }
 
--- src/component/ToDoList.tsx
+++ src/component/ToDoList.tsx
@@ -1,23 +1,44 @@
-import React, { useState } from "react";
+import { useForm } from "react-hook-form";
+
+interface ToDoFormData {
+  toDo: string;
+}
 
 function ToDoList() {
-  const [toDoInputValue, setToDoInputValue] = useState("");
+  const {
+    register, setValue, handleSubmit, watch,
+    formState: { errors }
+  } = useForm<ToDoFormData>({
+    defaultValues: {
+      toDo: "",
+    }
+  });
+  const watchFormData = watch();
 
-  const onChangeToDoInput = (event: React.FormEvent<HTMLInputElement>) => {
-    const { currentTarget: { value } } = event;
-    setToDoInputValue(value);
+  const onSubmit = (data: ToDoFormData) => {
+    window.alert(data.toDo);
+    setValue("toDo", "");
   };
 
-  const onSubmitToDoAddForm = (event: React.FormEvent<HTMLFormElement>) => {
-    event.preventDefault();
-    console.log(toDoInputValue);
-  };
+  console.log(watchFormData);
 
   return (
     <div>
-      <form onSubmit={onSubmitToDoAddForm}>
-        <input onChange={onChangeToDoInput} value={toDoInputValue} placeholder="Write here..."/>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <input
+          {...register("toDo", {
+            required: "Please enter a To Do",
+            maxLength: {
+              value: 20,
+              message: "Max 20 characters allowed"
+            }
+          })}
+          placeholder="Write a To Do..."
+          autoComplete="off"/>
         <button>Add</button>
+        <div style={{ color: "#f00" }}>
+          {errors.toDo?.message}
+        </div>
       </form>
     </div>
   );
